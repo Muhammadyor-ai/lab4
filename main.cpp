b4e94d01b6bc9b5d502c3e4163b9fb93034e813c
250147 <250147@newuu.uz>
2025-09-09 12:49:16 +0500

    my first commit

---
+++ main.cpp
@@ -0,0 +1,50 @@
+//problem 1
+//#include <iostream>
+
+//int main() {
+//    int intNumber=24;
+//    float floatNumber=3.1415;
+//    double doubleNumber=45.1234;
+//    bool boolean=true;
+//    char charName='A';
+//    std::cout << "Value of the Integer is "<<intNumber<<". Size is "<<sizeof(intNumber)<<"\n";
+//    std::cout << "Value of the Float is "<<floatNumber<<". Size is "<<sizeof(floatNumber)<<"\n";
+//    std::cout << "Value of the Double is "<<doubleNumber<<". Size is "<<sizeof(doubleNumber)<<"\n";
+//    std::cout << "Value of the Char is "<<charName<<". Size is "<<sizeof(charName)<<"\n";
+//    std::cout << "Value of the Bool is "<<boolean<<". Size is "<<sizeof(boolean)<<"\n";
+//    return 0;
+//}
+//problem 2
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     string name="Muhammadyorbek";
+//     string id = "250147";
+//     string song = "Despacito";
+//     string movie = "Avengars";
+//     cout <<name<<endl;
+//     cout <<id<<endl;
+//     cout <<song<<endl;
+//     cout <<movie<<endl;
+//     return 0;
+// }
+//problem 3
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     cout << " \"I\'m standing\" on the edge of some crazy cliff.\nWhat I have to do, I have to catch everybody\\ \n if they start to go over the cliff-I mean\\ \n  \\if they\'re running and they don\'t look\n  where they\'re going I have to come out\n  from somewhere and     catch them.\nRefer to escape sequences table. "<< endl;
+//     return 0;
+// }
+//problem 4
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int a=189;
+//     char b='B';
+//     float float_num_1=a/3;
+//     int dec_val_b=b;
+//     float float_num_2=(a+b)/5;
+//     int character_1=(dec_val_b)+(a/10-1);
+//     cout<<" "<<float_num_1<<" "<<float_num_2<<" "<<dec_val_b<<" "<<character_1<<" ";
+//     return 0;
+// }
