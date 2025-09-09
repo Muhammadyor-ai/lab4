e9aacea5dcc580c09123e7ed2dc0120801e813da
250147 <250147@newuu.uz>
2025-09-09 19:28:48 +0500

    my first commit

---
+++ main.cpp
@@ -0,0 +1,111 @@
+//problem 1
+//#include <iostream>
+
+//int main() {
+//    int intNumber=24;
+//    float floatNumber=3.1415;
+//    double doubleNumber=45.1234;
+//    bool boolean=true;
+//    char charName='A';
+//    std::cout << "Value of the Integer is "<<intNumber<<". Size is "<<sizeof(intNumber)<<"\n";
+//    std::cout << "Value of the Float is "<<floatNumber<<". Size is "<<sizeof(floatNumber)<<"\n";
+//    std::cout << "Value of the Double is "<<doubleNumber<<". Size is "<<sizeof(doubleNumber)<<"\n";
+//    std::cout << "Value of the Char is "<<charName<<". Size is "<<sizeof(charName)<<"\n";
+//    std::cout << "Value of the Bool is "<<boolean<<". Size is "<<sizeof(boolean)<<"\n";
+//    return 0;
+//}
+//problem 2
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     string name="Muhammadyorbek";
+//     string id = "250147";
+//     string song = "Despacito";
+//     string movie = "Avengars";
+//     cout <<name<<endl;
+//     cout <<id<<endl;
+//     cout <<song<<endl;
+//     cout <<movie<<endl;
+//     return 0;
+// }
+//problem 3
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     cout << " \"I\'m standing\" on the edge of some crazy cliff.\nWhat I have to do, I have to catch everybody\\ \n if they start to go over the cliff-I mean\\ \n  \\if they\'re running and they don\'t look\n  where they\'re going I have to come out\n  from somewhere and     catch them.\nRefer to escape sequences table. "<< endl;
+//     return 0;
+// }
+//problem 4
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int a=189;
+//     char b='B';
+//     float float_num_1=a/3;
+//     int dec_val_b=b;
+//     float float_num_2=(a+b)/5;
+//     int character_1=(dec_val_b)+(a/10-1);
+//     cout<<" "<<float_num_1<<" "<<float_num_2<<" "<<dec_val_b<<" "<<character_1<<" ";
+//     return 0;
+// }
+//problem 5
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int a=12;
+//     int b=38;
+//     int Sum=a+b;
+//     int Product=a*b;
+//     cout<<"Sum="<<Sum<<"\nProduct="<<Product<<"";
+//     return 0;
+// }
+// problem 6
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int value1, value2;
+//     cin >>value1>>value2;
+//     int result = value1 + value2;
+//     cout << "Sum: "<<value1<<"+"<<value2<<"="<<result<<""<<endl;
+//     return 0;
+// }
+// problem 7
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     float x, y;
+//     cin>>x>>y;
+//     float t=(3*(x*x+3))/(y/4+3);
+//     cout<<"t="<<t<<endl;
+//     return 0;
+// }
+// problem 8
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     float d;
+//     cin>>d;
+//     const double PI = 3.141592653589;
+//     double area=(d*d/4)*PI;
+//     cout<<"Area: "<<area<<""<<endl;
+// }
+// problem 9
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     float v0, v1, t;
+//     cin>>v0>>v1>>t;
+//     cout<<(v0-v1)/t;
+//     return 0;
+// }
+// problem 10
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     float distance, fuel_eff, price;
+//     cin>>distance>>fuel_eff>>price;
+//     float volume = distance/fuel_eff;
+//     float cost = volume * price;
+//     cout<<"The cost of driving is $"<<cost<<"";
+//     return 0;
+// }
