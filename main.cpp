ffe232979dd7e6ff1feec55a09048824b0196eea
250147 <250147@newuu.uz>
2025-09-11 12:39:56 +0500

    problems 11-18

---
+++ main.cpp
@@ -0,0 +1,90 @@
+//problem 11
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     double a=1.13e3, b=411e-4;
+//     cout<<a<<" "<<b<<endl;
+//     return 0;
+// }
+//problem 12
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int all;
+//     cin >>all;
+//     int first_group=all/11;
+//     int last_group=all%11;
+//     cout<<"In first group:"<<first_group<<"\nIn the last group:"<<last_group<<endl;
+//     return 0;
+// }
+//problem 13
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     float side;
+//     cin >> side;
+//     float area=(3*1.73)/2*side*side;
+//     cout << "The area of the hexagon:"<<area << endl;
+//     return 0;
+// }
+//problem 14
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int num;
+//     cin >> num;
+//     num++;
+//     num*=3;
+//     num-=14;
+//     num/=4;
+//     num--;
+//     num%=9;
+//     cout << num << endl;
+//     return 0;
+// }
+//problem 15
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     char a='A';
+//     a+=7;
+//     cout<<a;
+//     a-=3;
+//     cout<<a;
+//     a+=7;
+//     cout<<a<<a;
+//     a+=3;
+//     cout<<a;
+//     a-=46;
+//     cout<<a<<endl;
+//     return 0;
+// }
+//problem 16
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     float x, y;
+//     cin >> x >> y;
+//     float z=(x+x+1)/((y+1)*(y+2));
+//     cout << z << endl;
+//     return 0;
+// }
+//problem 17
+// #include <iostream>
+// using namespace std;
+// int main () {
+//     int a, b;
+//     cin >> a >> b;
+//     cout << (a==b) << endl;
+//     return 0;
+// }
+//problem 18
+// #include <iostream>
+// using namespace std;
+// int main() {
+//    int a, b;
+//    cin >> a >> b;
+//    cout << " "<<(a<140 and a<b) << endl;
+//    return 0;
+// }
+//pro
