4faf9708b811e3d460bd5bd92ac05853efbca8ae
250147 <250147@newuu.uz>
2025-09-12 09:49:15 +0500

    problem 19-22

---
+++ main.cpp
@@ -0,0 +1,40 @@
+//problem 19
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int a, b, c;
+//     cin >> a >> b >> c;
+//     cout << (a>b and a>c);
+//     return 0;
+// }
+//problem 20
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int a, b, c, d, e;
+//     cin >> a >> b >> c >> d >> e;
+//     cout << (a>=0 or b>=0 or c>=0 or d>=0 or e>=0) << endl;
+//     return 0;
+// }
+//problem 21
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int a, b;
+//     cin >> a >> b;
+//     int c=10-b;
+//     cout << ((a+b)>30 and a*b>30)<<" ";
+//     cout << (a+c>30 or a*c>30)<<" ";
+//     cout << (a>30 or b>30);
+//     return 0;
+// }
+// //problem 22
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     float s, m;
+//     cin>>s>>m;
+//     float a=30.48*m;
+//     cout<<(a>s);
+//     return 0;
+// }
